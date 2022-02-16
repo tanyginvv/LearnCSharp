1d8b9ac6b5139bfa2e34e7ca7a1d24de282c5860
Vadim <tanyginvv@mail.ru>
2022-02-16 21:58:11 +0300

    Initial commit

---
+++ LearnCSharp/Ellipse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TL1
+{
+    public class Ellipse
+    {
+        
+        public int SmallAxis { get; private set; }
+        public int BigAxis { get; private set; }
+
+        public Ellipse( int smallAxis, int bigAxis )
+        {
+            if ( smallAxis <= 0 )
+            {
+                throw new ArgumentException( "SmallAxis cannot be less than or equal to 0" );
+            }
+            if ( bigAxis <= 0 ) 
+            {
+                throw new ArgumentException("BigAxis cannot be less than or equal to 0");
+            }
+
+            SmallAxis = smallAxis;
+            BigAxis = bigAxis;
+        }
+
+
+        public int GetSquare()
+        {
+            return (int)( SmallAxis * BigAxis * Math.PI );
+        }
+        public int GetLength()
+        {
+            return (int)( ( SmallAxis + BigAxis) * Math.PI );
+        }
+    }
+}
---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using TL1;
+
+class Program
+{
+    public static void Main( string[] args ) 
+    {
+        int smallAxis = 5;
+        int bigAxis = 10;
+        try
+        {
+            Ellipse ellipse = new Ellipse( smallAxis, bigAxis );
+            int square = ellipse.GetSquare();
+            int length = ellipse.GetLength();
+            Console.WriteLine( $"Площадь эллипса: {ellipse.GetSquare()}" );
+            Console.WriteLine( $"Длина эллипса: {ellipse.GetLength()}" );
+        }
+        catch ( ArgumentException ex )
+        {
+            Console.WriteLine( ex.Message );
+        }
+    }
+}
