da4d7d52398b81c24a3eab8ddad0e7602dba9360
tanyginvv <99418463+tanyginvv@users.noreply.github.com>
2022-02-17 23:55:10 +0300

    Add files via upload
    Исправление

---
+++ Ellipse.cs
@@ -0,0 +1,32 @@
+﻿namespace TL1
+{
+    public class Ellipse
+    {
+        public int MinorAxis { get; private set; }
+        public int MajorAxis { get; private set; }
+
+        public Ellipse( int minorAxis, int majorAxis )
+        {
+            if ( minorAxis <= 0 )
+            {
+                throw new ArgumentException( "Minor semiaxis of ellipse cannot be less than or equal to 0" );
+            }
+            if ( majorAxis <= 0 ) 
+            {
+                throw new ArgumentException( "Major semiaxis of ellipse cannot be less than or equal to 0" );
+            }
+
+            MinorAxis = minorAxis;
+            MajorAxis = majorAxis;
+        }
+
+        public int GetSquare()
+        {
+            return ( int )( Math.PI * MinorAxis * MajorAxis );
+        }
+        public int GetLength()
+        {
+            return ( int )( Math.PI * (MinorAxis + MajorAxis) );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using TL1;
+
+class Program
+{
+    public static void Main( string[] args ) 
+    {
+        int minorAxis = 5;
+        int majorAxis = 10;
+
+        try
+        {
+            Ellipse ellipse = new Ellipse( minorAxis, majorAxis );
+            int square = ellipse.GetSquare();
+            int length = ellipse.GetLength();
+            Console.WriteLine( $"Площадь эллипса: {ellipse.GetSquare()}" );
+            Console.WriteLine( $"Длина эллипса: {ellipse.GetLength()}" );
+        }
+        catch ( ArgumentException ex )
+        {
+            Console.WriteLine( ex.Message );
+        }
+    }
+}
